ec4ff48ea6bcd8530a189c199def382353ced78c
Mujtabasv <123363181+Mujtabasv@users.noreply.github.com>
2024-02-04 21:51:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import numpy as np 
+from PIL import Image
+import cv2
+
+
+# Making Title and header.
+
+st.title('Face Detection Model.')
+st.header('Detect face in uploaded image file.')
+
+model = cv2.CascadeClassifier("C:/Users/mujta/Downloads/haarcascade_frontalface_default.xml")
+
+user_img = st.file_uploader('Upload an Image!',type=['jpg','png','jpeg'])
+
+
+
+if user_img is not None:
+    with open("Image.jpg","wb") as f:
+        f.write(user_img.read())
+
+if user_img:   
+    img_arr=cv2.imread("Image.jpg")
+    
+    gray_img = cv2.cvtColor(img_arr, cv2.COLOR_RGB2GRAY)
+    faces = model.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=8)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img_arr, (x, y), (x+w, y+h), (0, 0, 256), 5)  
+    
+    st.image(img_arr, caption='Image with rectangle')
